7e0e4b115cfaeee0d00094b06be664dd12f5eb7a
Uemerson Pinheiro Junior <uemersonpinheirojunior@gmail.com>
2022-05-18 14:13:51 -0300

    feat: add pages and components

---
+++ components/Footer/index.tsx
@@ -0,0 +1,12 @@
+const Footer = () => (
+  <footer className="footer">
+    <a
+      href="https://github.com/Uemerson/uemerson.github.io"
+      className="link footer__link"
+    >
+      Made with ❤ by Uemerson Pinheiro Junior
+    </a>
+  </footer>
+);
+
+export default Footer;
---
+++ utility/portfolio.ts
@@ -0,0 +1,110 @@
+const header = {
+  homepage: 'https://github.com/Uemerson',
+  title: 'UP.',
+};
+
+const about = {
+  resume: '',
+  name: 'Uemerson Pinheiro Junior',
+  role: 'Software Engineer',
+  description:
+    'I\'m currently doing a master\'s degree in computer science at São Paulo State University "Júlio de Mesquita Filho" (UNESP) in the area of computational intelligence research. I working on <a href="https://www.datah.ai/" target="_blank" class="link">DATA H: AI driving Human innovation seedling.</a> I’m learning Machine learning, Deep learning, Clean Code, Software Architecture.',
+  social: {
+    linkedin: 'https://www.linkedin.com/in/uemerson-pinheiro-junior-953a38170/',
+    github: 'https://github.com/Uemerson',
+  },
+};
+
+const projects = [
+  {
+    name: 'Clean Python API',
+    description:
+      'API in Flask using Python, TDD, Clean Architecture, Design Patterns and SOLID principles.',
+    stack: [
+      'Python',
+      'Flask',
+      'TDD',
+      'Clean Architecture',
+      'SOLID',
+      'Design Patterns',
+    ],
+    sourceCode: 'https://github.com/Uemerson/clean-python-api',
+    livePreview: '',
+  },
+  {
+    name: 'Clean Node API',
+    description:
+      'API in NodeJs using JavaScript, TDD, Clean Architecture, Design Patterns and SOLID principles',
+    stack: [
+      'JavaScript',
+      'Node.js',
+      'TDD',
+      'Clean Architecture',
+      'SOLID',
+      'Design Patterns',
+    ],
+    sourceCode: 'https://github.com/Uemerson/clean-node-api',
+    livePreview: '',
+  },
+  {
+    name: 'Bidirectional LSTM Bitcoin Prices',
+    description:
+      'Multivariate Time Series Forecasting with LSTM and Bidirectional LSTM to predict Bitcoin Prices',
+    stack: ['LSTM', 'Deep Learning', 'Python', 'Jupyter Notebook'],
+    sourceCode: 'https://github.com/Uemerson/bidirectional-lstm-bitcoin-prices',
+    livePreview: '',
+  },
+  {
+    name: 'CyberMakers Network',
+    description:
+      "Contribution to the site of CyberMakers Network. CyberMakers Network it's non-financial to be an open-source and cooperative organization.",
+    stack: ['Next.js', 'TypeScript', 'CSS'],
+    sourceCode:
+      'https://github.com/CyberMakers-Network/CyberMakers-Network.github.io',
+    livePreview: 'https://cybermakers-network.github.io/',
+  },
+];
+
+const skills = [
+  'Python ❤',
+  'JavaScript',
+  'TypeScript',
+  'Java',
+  'C#',
+  'Visual Basic',
+  'VB.NET',
+  'HTML',
+  'CSS',
+  'SASS',
+  'Material UI',
+  'Django',
+  'Flask',
+  'Node.js',
+  'React.js',
+  'React Native',
+  'Next.js',
+  'Vue.js',
+  'Angular',
+  'Ionic',
+  'Git',
+  'GitHub',
+  'GitLab',
+  'CI/CD',
+  'TDD',
+  'Jest',
+  'Pytest',
+  'Python Unittest',
+  'Digital Ocean',
+  'Firebase',
+  'AWS S3',
+  'Spaces',
+  'MongoDB',
+  'PostgreSQL',
+  'And others ...',
+];
+
+const contact = {
+  email: 'uemersonpinheirojunior@gmail.com',
+};
+
+export { header, about, projects, skills, contact };
