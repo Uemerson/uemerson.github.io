461f1dfd54a20f6c7b43b16fdb55eaff24e5e227
Uemerson Pinheiro Junior <uemersonpinheirojunior@gmail.com>
2022-05-20 22:08:27 -0300

    feat: add head

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,23 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 import AppProvider from '../contexts';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <AppProvider>
-      <Component {...pageProps} />
-    </AppProvider>
+    <>
+      <Head>
+        <link rel="shortcut icon" href={'/favicon.ico'} />
+        <title>Uemerson Pinheiro Junior</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+      </Head>
+      <AppProvider>
+        <Component {...pageProps} />
+      </AppProvider>
+    </>
   );
 }
 
